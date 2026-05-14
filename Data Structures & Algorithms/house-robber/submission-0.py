12cb4db2e1da0400bff639b7358e808bdf4f78e4
||-MATRIX-|| <ekantbajaj1@gmail.com>
2026-05-14 09:16:41 +0530

    Add: house-robber - submission-0

---
+++ Data Structures & Algorithms/house-robber/submission-0.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        # d[i] is the maximum amount i have on till index i
+        # choice is that i can skip this house or rob this house
+        # transition max(dp[i-1], num[i]+dp[i-2])
+        n = len(nums)
+        dp = [0]*(n+2)
+        for i in range(n-1,-1,-1):
+            dp[i]=max(dp[i+1],nums[i]+dp[i+2])
+        print(dp)
+        return dp[0]
