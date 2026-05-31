55d86efbac35e6c2e445e307a81b44ddfce35154
||-MATRIX-|| <ekantbajaj1@gmail.com>
2026-05-31 12:02:47 +0530

    Add: combination-target-sum-ii - submission-0

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-0.py
@@ -0,0 +1,27 @@
+class Solution:
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        
+
+        res = []
+        sub = []
+        candidates.sort()
+
+        def generate(i,total):
+            if total==target:
+                res.append(sub.copy())
+                return
+            if i>=len(candidates) or total>target:
+                return
+             # Exclude current value and all its duplicates
+            j = i
+            while j + 1 < len(candidates) and candidates[j] == candidates[j + 1]:
+                j += 1
+
+            generate(j + 1, total)
+            sub.append(candidates[i])
+            generate(i+1,total+candidates[i])
+            sub.pop()
+        
+        generate(0,0)
+
+        return res
